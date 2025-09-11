c07b825bf0d2419952a04b80fd2839ab275f1048
Karthikeyan Thirunavukkarasu <thirunavukarasur759@gmail.com>
2025-09-11 21:33:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "5a8167a617848da1772063ce8a****68";
+const apiKey = "5a8167a6178****1772063ce8a****68";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
